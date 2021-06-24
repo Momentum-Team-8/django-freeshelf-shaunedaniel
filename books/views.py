e1bb8436ba55ff4e4aaddda746289774f5201a92
shaunedaniel <75916901+shaunedaniel@users.noreply.github.com>
2021-06-23 21:29:45 -0400

    ManyToManyField error :(

--- books/views.py
+++ books/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Book
+from .models import Category
+
+
+def homepage(request):
+
+    if request.user.is_authenticated:
+        return redirect("book_list")
+    return render(request, "books/homepage.html")
+
+
+@login_required
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "books/book_list.html",
+                  {"books": books})
+
+
+def show_categ(request, slug):
+    categ = get_object_or_404(Category, slug=slug)
+    books = categ.books.all()
+    return render(request, "books/show_category.html", {"category": category, "books": books})
